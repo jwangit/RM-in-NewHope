9285b46b8dab071d49d69f98ef7ec8efc2d538d6
jwang <wangjiabo90@163.com>
2020-05-06 17:18:37 +0100

    simplified channel model without encryption and decryption

---
+++ reedmullerO3/reedmullergmc.h
@@ -0,0 +1,34 @@
+//
+//  reedmullergmc.h
+//
+//  Created by Jiabo Wang (Mar) on 11/03/20.
+//  Copyright © 2020 Wang, Jiabo (Mar). All rights reserved.
+//
+#ifndef REEDMULLERGMC_H
+#define REEDMULLERGMC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+
+typedef struct treeNode
+{
+    uint8_t r, m;
+    double *recYv;
+    double *recYu;
+    int *chat;
+//    int8_t *mhat;
+    struct treeNode *lchild, *rchild;
+}Btree;
+
+Btree *createTree(double *ptr, int8_t r, int8_t m);
+void preorder(Btree *T);
+void destroyTree(Btree *T);
+
+int sign(double x);
+double* rm_calc_f(double *recY, uint16_t n);
+double* rm_calc_g(double *recY, uint16_t n, uint8_t *a1hat);
+int *combsubcodes (Btree *Tv, Btree *Tu, int N);
+
+#endif
